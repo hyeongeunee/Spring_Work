35ee8272e0b6762fb49a2c7f4270b93de02726bd
hyeongeun <hhg7899@gmail.com>
2023-06-29 17:56:17 +0900

    게시판 기능 ++

--- Spring04_Final/src/main/java/com/gura/spring/cafe/dao/CafeDaoImpl.java
+++ Spring04_Final/src/main/java/com/gura/spring/cafe/dao/CafeDaoImpl.java
@@ -9,46 +9,52 @@ import java.util.List;
 
 @Repository
 public class CafeDaoImpl implements CafeDao {
+
     @Autowired
     private SqlSession session;
 
     @Override
     public List<CafeDto> getList(CafeDto dto) {
-        return null;
+
+        return session.selectList("cafe.getList", dto);
     }
 
     @Override
     public int getCount(CafeDto dto) {
-        return 0;
+
+        return session.selectOne("cafe.getCount", dto);
     }
 
     @Override
     public void insert(CafeDto dto) {
-
+        session.insert("cafe.insert", dto);
     }
 
     @Override
     public CafeDto getData(int num) {
-        return null;
+
+        return session.selectOne("cafe.getData", num);
     }
 
     @Override
     public CafeDto getData(CafeDto dto) {
-        return null;
+
+        return session.selectOne("cafe.getData2", dto);
     }
 
     @Override
     public void addViewCount(int num) {
-
+        session.update("cafe.addViewCount", num);
     }
 
     @Override
     public void delete(int num) {
-
+        session.delete("cafe.delete", num);
     }
 
     @Override
     public void update(CafeDto dto) {
-
+        session.update("cafe.update", dto);
     }
+
 }
