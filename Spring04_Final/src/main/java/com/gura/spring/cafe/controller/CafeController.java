7fc3704428ad11f50584a6a3203ee11f96a625c3
hyeongeun <hhg7899@gmail.com>
2023-06-30 17:48:53 +0900

    댓글기능-ing

--- Spring04_Final/src/main/java/com/gura/spring/cafe/controller/CafeController.java
+++ Spring04_Final/src/main/java/com/gura/spring/cafe/controller/CafeController.java
@@ -14,6 +14,30 @@ public class CafeController {
     @Autowired
     private CafeService service;
 
+    // 댓글 더보기 요청 처리
+    @RequestMapping("/cafe/ajax_comment_list")
+    public String commentList(HttpServletRequest request) {
+        // 테스트를 위해 시간 지연 시키기
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        service.moreCommentList(request);
+
+        return "cafe/ajax_comment_list";
+    }
+
+    // 새로운 댓글 저장 요청 처리
+    @RequestMapping("/cafe/comment_insert")
+    public String commentInsert(HttpServletRequest request, int ref_group) {
+        // 새로운 댓글을 저장하는 로직을 수행한다.
+        service.saveComment(request);
+        // ref_group 은 원글의 글 번호이기 때문에 원글 자세히 보기로 다시 리다일렉트 이동된다.
+        return "redirect:/cafe/detail?num=" + ref_group;
+    }
+
     @RequestMapping("/cafe/list")
     public String list(HttpServletRequest request) {
         // 서비스에 HttpServletRequest 객체를 전달해서 응답에 필요한 데이터가 담기도록 하고
